951b4ec2de03d6551b6fde05275b7e87daa1de16
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-18 15:20:12 +0900

    header.phpの修正

--- views/component/header.php
+++ views/component/header.php
@@ -3,43 +3,29 @@ require_once(__DIR__ . '/../../controllers/UserController.php');
 $userController = new UserController();
 ?>
 
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-    <link rel="stylesheet" type="text/css" href="../../frontend/style.css">
-</head>
-
-<body>
-    <div class="header">
-        <nav>
-            <?php if ($userController->isUserLoggedIn()) : ?>
-                <!-- ログイン済み -->
-                <?php if (basename($_SERVER['PHP_SELF']) === 'profile.php') : ?>
-                    <!-- profile.php の画面にいる場合 -->
-                    <a href="/views/logout.php">ログアウト</a>
-                <?php elseif (isset($_SESSION['userProfile'])) : ?>
-                    <!-- プロフィールが登録完了している場合 -->
-                    <a href="../../users/profile.php">プロフィール</a>
-                    <span>|</span>
-                    <a href="/views/logout.php">ログアウト</a>
-                <?php else : ?>
-                    <!-- プロフィール未登録でログイン済み -->
-                    <!-- <a href="profile.php">プロフィール</a>
-                    <span>|</span> -->
-                    <a href="/views/logout.php">ログアウト</a>
-                <?php endif; ?>
-            <?php else : ?>
-                <!-- 未ログイン -->
-                <a href="login.php">ログイン</a>
+<div class="header">
+    <nav>
+        <?php if ($userController->isUserLoggedIn()) : ?>
+            <!-- ログイン済み -->
+            <?php if (basename($_SERVER['PHP_SELF']) === 'profile.php') : ?>
+                <!-- profile.php の画面にいる場合 -->
+                <a href="/views/logout.php">ログアウト</a>
+            <?php elseif (isset($_SESSION['userProfile'])) : ?>
+                <!-- プロフィールが登録完了している場合 -->
+                <a href="../../users/profile.php">プロフィール</a>
                 <span>|</span>
-                <a href="register.php">ユーザー登録</a>
+                <a href="/views/logout.php">ログアウト</a>
+            <?php else : ?>
+                <!-- プロフィール未登録でログイン済み -->
+                <!-- <a href="profile.php">プロフィール</a>
+                    <span>|</span> -->
+                <a href="/views/logout.php">ログアウト</a>
             <?php endif; ?>
-        </nav>
-    </div>
-</body>
-
-</html>
+        <?php else : ?>
+            <!-- 未ログイン -->
+            <a href="login.php">ログイン</a>
+            <span>|</span>
+            <a href="register.php">ユーザー登録</a>
+        <?php endif; ?>
+    </nav>
+</div>
